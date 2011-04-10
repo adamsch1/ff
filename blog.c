f7ec3b6b740fb7e41e9a66db1e8dba2b45d67ffa
adamsch1 <adamsch1@yahoo.com>
2011-04-10 09:44:29 -0700

    Comments for blog

--- blog.c
+++ blog.c
@@ -5,16 +5,28 @@
 void blog();
 void comment();
 
+/**
+ * Tell ff what we provide
+ */
 struct ff_controller_t head[] = {
   { "/blog", blog  },
   { "/comments", comment },
   { 0 }
 };
 
+/**
+ * Initialize template cache
+ */
 void init()  {
   template_init();
 }
 
+/**
+ * Ubiquitous, dumbass blog 
+ * 
+ * Going forward - don't allocate memory if possiblem ask FF for resource
+ * it can free everything after the call
+ */
 void blog()  {
   struct array_t *arr = array_new();
 
